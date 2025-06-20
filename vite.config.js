a7bcac86ee2c34c209a049591e7e58bdf3002e68
juant8560 <microfonos-broncear-0f@icloud.com>
2025-06-20 14:07:27 -0600

    Subida de Proyecto

---
+++ vite.config.js
@@ -0,0 +1,42 @@
+import { defineConfig } from "vite";
+import htmlPurge from 'vite-plugin-purgecss';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
+import path, {resolve} from 'node:path';
+import * as glob from 'glob';
+
+const obtenerEntradas = ()=> {
+    return Object.fromEntries(
+        [
+            ...glob.sync(
+                './**/*.html',
+                {
+                    ignore: [
+                        './dist/**',
+                        './node_modules/**'
+                    ]
+                }
+            ).map(
+                file => [
+                    file.slice(0, file.length - path.extname(file).length),
+                    resolve(__dirname, file)
+                ]
+            )
+        ]
+    );
+}
+
+export default defineConfig(
+    {
+        appType: 'mpa',
+        build: {
+            rollupOptions: {
+                input: obtenerEntradas()
+            },
+            minify: true
+        },
+        plugins: [
+            htmlPurge({}),
+            ViteMinifyPlugin(),
+        ]
+    }
+);
